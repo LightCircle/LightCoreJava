52ddb2ecdf3ecd0c6c55b144efc048fba9114f9e
LI LIN <r2space@gmail.com>
2016-11-10 16:48:48 +0800

    添加jquery.param()格式querystring的解析方法

--- src/test/java/cn/alphabets/light/HelperTest.java
+++ src/test/java/cn/alphabets/light/HelperTest.java
@@ -1,5 +1,6 @@
 package cn.alphabets.light;
 
+import cn.alphabets.light.model.Json;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -12,6 +13,36 @@ import java.util.concurrent.ConcurrentHashMap;
  * HelperTest
  */
 public class HelperTest {
+
+    @Test
+    public void testUnParam() {
+
+        String slimjson = "[ \"]*";
+        Json source = Helper.unParam("s=0&c[m][]=a&c[m][]=b&c[p][p1][p11]=d&c[p][p1][p12][]=1&c[p][p1][p12][]=2");
+        String json = source.toJson().replaceAll(slimjson, "");
+
+        Assert.assertEquals(json, "{s:0,c:{m:[a,b],p:{p1:{p11:d,p12:[1,2]}}}}");
+    }
+
+    @Test
+    public void testLookup() {
+
+        String slimjson = "[ \"]*";
+
+        Json source = new Json();
+        Helper.setValueByJsonPath(source, Arrays.asList("key", ""), 1);
+        Helper.setValueByJsonPath(source, Arrays.asList("key", ""), 2);
+        Assert.assertEquals(source.toJson().replaceAll(slimjson, ""), "{key:[1,2]}");
+
+        source = new Json();
+        Helper.setValueByJsonPath(source, Arrays.asList("key", "son"), 1);
+        Assert.assertEquals(source.toJson().replaceAll(slimjson, ""), "{key:{son:1}}");
+
+        source = new Json();
+        Helper.setValueByJsonPath(source, Arrays.asList("key", "0", "son"), "value1");
+        Helper.setValueByJsonPath(source, Arrays.asList("key", "1", "son"), "value2");
+        Assert.assertEquals(source.toJson().replaceAll(slimjson, ""), "{key:[{son:value1},{son:value2}]}");
+    }
 
     @Test
     public void testLoadTemplate() throws IOException {
