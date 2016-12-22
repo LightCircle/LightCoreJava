ad6497a3ad2896507a63835ef5039301203e82fe
fzcs <fzcs@live.cn>
2016-12-22 09:33:20 +0800

    调用controller方法失败时输出详细信息

--- src/main/java/cn/alphabets/light/http/Dispatcher.java
+++ src/main/java/cn/alphabets/light/http/Dispatcher.java
@@ -254,6 +254,7 @@ public class Dispatcher {
             try {
                 this.methods.put(key, Class.forName(fullName).getMethod(methodName, Context.class));
             } catch (NoSuchMethodException | ClassNotFoundException e) {
+                logger.warn("Class Method : " + key + " not found", e);
                 return null;
             }
         }
